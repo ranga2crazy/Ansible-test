47b1d8d06cf84bb07c080e03c22785ada32da7ee
Ranganathan Ragothaman <ranga2crazy@gmail.com>
2018-02-19 16:22:11 +0530

    .

--- telnet.sh
+++ telnet.sh
@@ -1,13 +1,12 @@
 date
-echo "job started"
 git clone https://github.com/ranga2crazy/Ansible-test.git
 cd Ansible-test
 git pull
 mkdir /home/ec2-user/temp
-cp install_telnet.yml telnet.sh /home/ec2-user/temp/
+cp Tomcat_install_start.yml telnet.sh /home/ec2-user/temp/
 cd /home/ec2-user/temp/
 chmod 755 *
-ansible-playbook install_telnet.yml -vvv
+ansible-playbook Tomcat_install_start.yml -vvv
 cd
 rm -rf temp Ansible-test
 echo done
