b8c699c7a1e78a0e23228914b2856e75a82f5261
Ranganathan Ragothaman <ranga2crazy@gmail.com>
2018-02-19 16:23:50 +0530

    .

--- Commands.sh
+++ Commands.sh
@@ -3,7 +3,7 @@ git clone https://github.com/ranga2crazy/Ansible-test.git
 cd Ansible-test
 git pull
 mkdir /home/ec2-user/temp
-cp Tomcat_install_start.yml telnet.sh /home/ec2-user/temp/
+cp Tomcat_install_start.yml Commands.sh /home/ec2-user/temp/
 cd /home/ec2-user/temp/
 chmod 755 *
 ansible-playbook Tomcat_install_start.yml -vvv
